dd81f42c0b37dd27971c2cc716a1e0866ffab995
Ronaldo Victor <ronaldovctor@outlook.com>
2022-06-04 11:21:40 -0300

    Interpolacao

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./first-component.component.css']
 })
 export class FirstComponentComponent implements OnInit {
+  name: string = 'Ronaldo'
+  age: number = 21
+  job: string = 'Programador'
+  hobbies: string[] = ['Correr', 'Jogar', 'Estudar']
+  car = {
+    name: 'Mustang GT V12',
+    brand: 'Ford',
+    year: 2022
+  }
 
   constructor() { }
 
